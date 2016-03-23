b37b3a223f7d4c28bc856297055ed01578e48908
Simon Madine <simon.madine@here.com>
2016-03-23 15:17:57 +0100

    Add regex matching

--- tests/demo-postcode.sh
+++ tests/demo-postcode.sh
@@ -8,7 +8,14 @@ assert_status_code "http://api.postcodes.io/random/postcodes" "200"
 
 writeout "Test 2: Does the Postcodes API return the Queen's address?"
 haystack="`curl -s http://api.postcodes.io/postcodes/SW1A1AA `"
-needle='"admin_district":"Westminster","admin_ward":"St James'"'"'s"'
+needle='"admin_district":"Westminster"'
 assert_contains "$haystack" "$needle"
+needle='"admin_ward":"St James'"'"'s"'
+assert_contains "$haystack" "$needle"
+
+writeout "Test 3: Does the Postcodes API return a valid postcode?"
+haystack="`curl -s http://api.postcodes.io/random/postcodes `"
+match=[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9]..
+assert_matches "$haystack" "$match"
 
 result $EXIT_STATUS
--- utils.sh
+++ utils.sh
@@ -3,6 +3,41 @@ function writeout {
     echo -e "$1"
 }
 
+declare -a on_exit_items
+
+function on_exit()
+{
+    for i in "${on_exit_items[@]}"
+    do
+        # echo "on_exit: $i"
+        eval $i
+    done
+}
+
+function add_on_exit()
+{
+    local n=${#on_exit_items[*]}
+    on_exit_items[$n]="$*"
+    if [[ $n -eq 0 ]]; then
+        # echo "Setting trap"
+        trap on_exit EXIT
+    fi
+}
+
+function assert_matches()
+{
+    # bit of a hack to slurp haystacks with spaces
+    haystack=`echo $* | rev | cut -d" " -f2- | rev`
+    match=`echo $* | rev | cut -d" " -f-1 | rev`
+    if [[ $haystack =~ $match ]]; then
+        echo -e "\033[0;32mOK\033[0m"
+    else 
+        EXIT_STATUS=1;
+        echo -e "\033[0;31mExpected\033[0m: "$haystack
+        echo -e "\033[0;31mTo match\033[0m: "$match
+    fi
+}
+
 function assert_contains()
 {
     if [[ "$1" != *"$2"* ]]; then
@@ -28,7 +63,7 @@ function assert_equals()
 function assert_status_code()
 {
     if [[ "$1" != "" ]]; then
-        haystack="$(curl -s -w %{http_code} $1 -o /dev/null)"
+        haystack="`curl -s -o /dev/null -w '%{http_code}' $1`"
         assert_equals "$haystack" "$2"
     fi
 }
